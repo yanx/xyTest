d8a69252f96fff612ac4168b3f1e0510cb2e1b73
Yan Xin <yan@duxlaboratories.com>
2021-09-08 22:15:40 -0400

    Add files via upload
    init

---
+++ __init__.py
@@ -0,0 +1,10 @@
+
+__info__ = """
+    Name:          xyTest
+    Author:        Yan Xin
+    Description:   Test Automation Framework
+"""
+__product__ = 'XYTEST'
+__version__ = '0.1.0.1'
+__author__ = "Yan Xin"
+
